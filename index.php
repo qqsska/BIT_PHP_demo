f5bd639dd65484cede17db12e0029abd96733a23
Snaige <ssnaige@hotmail.com>
2020-04-27 22:12:57 +0300

    Files browser (on progress)
    Sprint 1 challenge.

--- index.php
+++ index.php
@@ -1,17 +1,81 @@
-
-
-<h3>DAUGINAME BET KOKIUS SKAICIUS</h3> 
-<form action = "" method= "post">
-    <input type= "text" name="n" value= "" placeholder="irasykite skaiciu"><br>
-    <input type= "text" name="n2" value= "" placeholder="irasykite skaiciu"><br>
-    <input type= "submit" name="act" value= "dauginti" >
-</form>
-<?php
-if(isset($_POST['act']))
-{
-    $n = $_POST['n'];
-    $n2 = $_POST['n2'];
-    $add = $n*$n2;
-    echo "Atsakymas : " . $add;
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+  border: 1px solid grey  
 }
-?>
+
+th, td {
+  text-align: left;
+  padding: 8px;
+  height: 20px;
+  border-left: 1px solid grey;
+  
+}
+   
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+  background-color: #4CAF50;
+  color: white;
+  
+}
+.button{
+color:  black;
+width: 235px;
+
+}
+</style>
+</head>
+<body>
+
+<br>
+<?php
+    $path = './'. $_GET['path'];
+    $files_and_dirs = scandir($path);
+
+    print ('<h2>Directory contents: ' . str_replace('?path+','',$_SERVER['REQUEST_URI']). '</h2>');
+
+    print('<table><th>Type</th><th>Name</th><th>Action</th>');
+    foreach ($files_and_dirs as $find){
+        if ($find != ",," and $find != ".") {
+            print('<tr>');
+            print('<td>' . (is_dir($path . $find) ? "Directory" : "file") . '</td>');
+            print('<td>' . (is_dir($path . $find) 
+                    ? '<a href="' . (isset($_GET['path'])
+                        ? $_SERVER['REQUEST_URI'] . $find . '/'
+                        : $_SERVER['REQUEST_URI'] . '?path=' . $find . '/') . '">' . $find . '</a>' 
+                  
+                    : $find)
+                . '</td>');
+               
+            print ('<td></td>');
+            print ('</tr>');
+
+
+        }
+    }
+    print ('</table>');
+
+    ?>
+
+<br><br>
+
+<form action="" method="POST">
+
+<button class="button"><a href="http://localhost/BIT_PHP_demo/sprint1/browser/">Back</a></button>
+<br><br>
+<button class="button">Choose file</a></button><br>
+<button class="button">Upload file</a></button><br>
+<form action="/action_page.php">
+  <input type="link" id="newName" name="lname" placeholder="Name of new directory">
+  <input type="submit">
+</form> 
+
+
+</body>
+</html>
+
